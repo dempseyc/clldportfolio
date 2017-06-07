e39410d2587e9bdb576f23f983b4a39f2e6459c2
Craig Dempsey <craigdempsey@gmail.com>
2017-06-06 21:13:01 -0400

    add more css

---
+++ projects.js
@@ -0,0 +1,26 @@
+let projects = [
+  {
+    "title": "Thinking Cat",
+    "thumbnail": "thinking-cat-thumb.gif",
+    "link": "#",
+    "github-repo": "theGithubRepo"
+  },
+  {
+    "title": "Our Moon Gardens",
+    "thumbnail": "our-moon-gardens-thumb.gif",
+    "link": "#",
+    "github-repo": "theGithubRepo"
+  },
+  {
+    "title": "Baad Plaanet!!",
+    "thumbnail": "baad-planet.gif",
+    "link": "#",
+    "github-repo": "theGithubRepo"
+  },
+  {
+    "title": "Elf Work",
+    "thumbnail": "elf-work.gif",
+    "link": "#",
+    "github-repo": "theGithubRepo"
+  }
+]
--- script.js
+++ script.js
@@ -3,34 +3,43 @@
   let b = 1;
   let w = window.innerWidth;
   let h = window.innerHeight;
-  let P = $('#printout');
+  // let P = $('#printout');
   let nSH = $('.n-shadow');
   let nuSH = $('.n-unshadow');
-  let rSH = $('.n-shadow');
-  let ruSH = $('.n-unshadow');
+  let rSH = $('.r-shadow');
+  let ruSH = $('.r-unshadow');
+  let eSH = $('.e-shadow');
+  let euSH = $('.e-unshadow');
   let sXAngle = 7;
-  let sYAngle = 7;
-  let usXAngle = 5;
+  let sYAngle = 5;
+  let usXAngle = 7;
   let usYAngle = 5;
   let body = $('body');
   let T = $('.transparent');
 
-
   body.on('mousemove', function(e) {
       a = e.clientY/h;
       b = e.clientX/w;
-      sYAngle = 4 + 3*a;
-      sXAngle = 5 + 1.5*b;
-      usYAngle = 5 - a;
-      usXAngle = 5 - 0.8*b;
-      nSH.css('top',sYAngle+'%');
+      sXAngle = 4 + 1.7*b;
+      sYAngle = 5 + 1.5*a;
+      usXAngle = 5 - 0.4*b;
+      usYAngle = 5 - 0.4*a;
       nSH.css('left',sXAngle+'%');
-      nuSH.css('top',usYAngle+'%');
+      nSH.css('top',sYAngle+'%');
       nuSH.css('left',usXAngle+'%');
-      rSH.css('top',sYAngle+'%');
+      nuSH.css('top',usYAngle+'%');
       rSH.css('left',sXAngle+'%');
-      ruSH.css('top',usYAngle+'%');
+      rSH.css('top',sYAngle+'%');
       ruSH.css('left',usXAngle+'%');
+      ruSH.css('top',usYAngle+'%');
+      eSH.css('left',sXAngle+'%');
+      eSH.css('top',sYAngle+'%');
+      euSH.css('left',usXAngle+'%');
+      euSH.css('top',usYAngle+'%');
   });
+
+  let Projs = $('#projects');
+  let projData = projects;
+  console.log(projData);
 
 })();
