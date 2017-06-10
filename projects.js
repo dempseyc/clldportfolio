af21e9ca582867da207930a506b1f52f0eab9024
Craig Dempsey <craigdempsey@gmail.com>
2017-06-10 13:17:15 -0400

    changed to gif jpg not showing in safari

--- projects.js
+++ projects.js
@@ -1,19 +1,19 @@
 let projects = [
   {
     "title": "Thinking Cat",
-    "thumbnail": "thinking_cat_thumb.jpeg",
+    "thumbnail": "thinking_cat_thumb.gif",
     "link": "https://github.com/dempseyc/thinking-cat",
     "github-repo": "https://github.com/dempseyc/thinking-cat"
   },
   {
     "title": "Our Moon Gardens",
-    "thumbnail": "our-moon-gardens-thumb2.jpeg",
+    "thumbnail": "our-moon-gardens-thumb2.gif",
     "link": "https://github.com/dempseyc/our_moon_gardens",
     "github-repo": "https://github.com/dempseyc/our_moon_gardens"
   },
   {
     "title": "Baad Plaanet!!",
-    "thumbnail": "baad_planet_thumb.jpeg",
+    "thumbnail": "baad_planet_thumb.gif",
     "link": "https://dempseyc.github.io/",
     "github-repo": "https://github.com/dempseyc/dempseyc.github.io"
   }
