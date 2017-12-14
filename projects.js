daf8dabb7d1a777b7299484c25983dd581291d44
Craig Dempsey <craigdempsey@gmail.com>
2017-12-14 14:16:24 -0500

    insert github links in projects.js

--- projects.js
+++ projects.js
@@ -3,19 +3,21 @@ var projects = [
     "title": "Thinking Cat",
     "heading": "personality test",
     "thumbnail": "thinking_cat_thumb.gif",
-    "link": "./thinking_cat_fe/index.html"
+    "link": "https://thinking-cat.herokuapp.com/",
+    "githublink": "https://github.com/dempseyc/thinking-cat"
   },
   {
     "title": "Our Moon Gardens",
     "heading": "visual playpen",
     "thumbnail": "our-moon-gardens-thumb2.gif",
-    "link": "https://our-moon-gardens.herokuapp.com/"
-    // "link": "https://github.com/dempseyc/our_moon_gardens"
+    "link": "https://our-moon-gardens.herokuapp.com/",
+    "githublink": "https://github.com/dempseyc/our_moon_gardens"
   },
   {
     "title": "Baad Plaanet!!",
     "heading": "rpg adventure engine",
     "thumbnail": "baad_planet_thumb.gif",
-    "link": "https://dempseyc.github.io/"
+    "link": "https://dempseyc.github.io/",
+    "githublink": ""
   }
 ]
